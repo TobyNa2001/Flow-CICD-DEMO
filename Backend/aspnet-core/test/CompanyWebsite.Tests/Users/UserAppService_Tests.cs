646a0f62142aa15595adca46812b50d55e9b6e0c
TobyNa2001 <110325784+TobyNa2001@users.noreply.github.com>
2023-07-17 13:39:58 +0800

    Update UserAppService_Tests.cs

--- Backend/aspnet-core/test/CompanyWebsite.Tests/Users/UserAppService_Tests.cs
+++ Backend/aspnet-core/test/CompanyWebsite.Tests/Users/UserAppService_Tests.cs
@@ -39,7 +39,7 @@ namespace CompanyWebsite.Tests.Users
                     Name = "John",
                     Surname = "Nash",
                     Password = "123qwe",
-                    UserName = "john.nyc"
+                    UserName = "john.nash"
                 });
 
             await UsingDbContextAsync(async context =>
